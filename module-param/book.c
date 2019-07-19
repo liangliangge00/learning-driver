d9d2f71bc7523c1ac467b1b0088440eb0aa5b6d7
Cqlismy <157823712@qq.com>
2019-07-19 23:56:05 +0800

    modified book.c

--- module-param/book.c
+++ module-param/book.c
@@ -10,7 +10,7 @@ module_param(book_num, int, S_IRUGO);
 static int __init book_init(void)
 {
     printk(KERN_INFO "book name:%s\n", book_name);
-    printk(KERN_INFO "book num:%d", book_num);
+    printk(KERN_INFO "book num:%d\n", book_num);
     
     return 0;
 }
