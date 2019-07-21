d10d17ac6ee1b135bff2e48ad9b2b4171045c3bd
Cqlismy <157823712@qq.com>
2019-07-21 14:45:51 +0800

    modified globalmem.c

--- globalmem/globalmem.c
+++ globalmem/globalmem.c
@@ -19,16 +19,49 @@ struct globalmem_dev {
 
 struct globalmem_dev *globalmem_devp;
 
-static void globalmem_setup_cdev(struct globalmem_dev *devp, int index)
+struct file_operations globalmem_fops {
+
+}
+
+static void globalmem_setup_cdev(struct globalmem_dev *dev, int index)
 {
     int err, devno = MKDEV(globalmem_major, index);
 
+    cdev_init(&dev->cdev, &gloablmem_fops);
+    dev->cdev.owner = THIS_MODULE;
+    err = cdev_add(&dev->cdev, devno, 1);
+    if (erro) {
+        printk(KERN_NOTICE "Error %d adding globalmem %d", err, index);
+    }
     
 }
 
 static int __init globalmem_init(void)
 {
+    int ret;
+    dev_t devno = MKDEV(globalmem_major, 0);
 
+    if (globalmem_major) {
+        ret = register_chrdev_region(devno, 1, "globalmem");
+    } else {
+        ret = alloc_chrdev_region(&devno, 0, 1, "globalmem");
+        globalmem_major = MAJOR(devno);
+    }
+    if (ret < 0)
+        return ret;
+
+    globalmem_devp = kzalloc(sizeof(struct globalmem_dev), GFP_KERNEL);
+    if (!globalmem_devp) {
+        ret = -ENOMEM;
+        goto fail_malloc;
+    }
+
+    globalmem_setup_cdev(globalmem_devp, 0);
+    return 0;
+
+    fail_malloc:
+    unregister_chrdev_region(devno, 1);
+    return ret;
 }
 
 static void __exit globalmem_exit(void)
