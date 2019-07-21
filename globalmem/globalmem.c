e01ef52ace57492c3108f83c47321acd6642cb86
Cqlismy <157823712@qq.com>
2019-07-21 20:01:46 +0800

    modified globalmem.c

--- globalmem/globalmem.c
+++ globalmem/globalmem.c
@@ -21,17 +21,29 @@ struct globalmem_dev *globalmem_devp;
 
 static int globalmem_open(struct inode *inode, struct file *filp)
 {
-
+    filp->private_data = globalmem_devp;
+    return 0;
 }
 
 static int globalmem_release(struct inode *inode, struct file *filp)
 {
-
+    return 0;
 }
 
 static long globalmem_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 {
+    struct globalmem_dev *dev = filp->private_data;
 
+    switch (cmd) {
+    case MEM_CLEAR:
+        memset(dev->mem, 0, GLOBALMEM_SIZE);
+        printk(KERN_INFO "globalmem is set to zero\n");
+        break;
+    default:
+        return -EINVAL;
+    }
+
+    return 0;
 }
 
 static ssize_t globalmem_read(struct file *filp, char __user *buf, size_t size, loff_t *ppos)
