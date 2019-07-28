e5819c9c243da69e2991765d8dcc2f4ed8c06ba2
Cqlismy <157823712@qq.com>
2019-07-28 20:59:09 +0800

    modified file

--- block/temp_wait_queue.c
+++ block/temp_wait_queue.c
@@ -5,20 +5,15 @@
 static ssize_t xxx_write(struct file *filp, const char *buffer, size_t count, loff_t *ppos)
 {
 	...
-<<<<<<< HEAD
-	DECLARE_WAITQUEUE(wait, current);	/* Define wait queue element */
+	DECLARE_WAITQUEUE(wait, current);	/* define wait queue element */
 	add_wait_queue(&xxx_wait, &wait);	/* add a new element to wait queue */
-=======
-	DECLARE_WAITQUEUE(wait, current);	/*define wait queue element*/
-	add_wait_queue(&xxx_wait, &wait);	/*add a new element to wait queue*/
->>>>>>> 47234ddca71121849f80fadd5b23a6aa3e9444f5
 
 	/* wait for the devices buffer to be writable */
 	do {
 		avail = device_writable(...);
 		if (avail < 0) {
 			if (filp->f_flags & O_NONBLOCK) {	/* no block process */
-				ret = -EAGAIN;		/*try again*/
+				ret = -EAGAIN;		/* try again */
 				goto out;
 			}
 			/* block process */
