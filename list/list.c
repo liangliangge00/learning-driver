bf2b74b8114134353a96f74cc72572e696d2aca7
Cqlismy <157823712@qq.com>
2019-07-29 16:49:05 +0800

    modified file

--- list/list.c
+++ list/list.c
@@ -9,26 +9,22 @@ struct task_list {
 int main(int argc, char *argv[])
 {
 	int i;
-	struct task_list *pos_ptr;
+	struct task_list *pos_ptr;	/* cursor */
+	struct list_head head_task;	/* list head */
+	struct task_list task[10];	/* ten elements for list*/
 
-	struct task_list head_task = {
-		.val = 0,
-		.my_list = LIST_HEAD_INIT(head_task.my_list),
-	};
-
-	struct task_list task[10];
-
+	INIT_LIST_HEAD(&head_task);	/* init head */
 	for (i = 0; i < 10; i++) {
 		task[i].val = i+1;
 		INIT_LIST_HEAD(&task[i].my_list);
-		list_add(&task[i].my_list, &head_task.my_list);
+		list_add(&task[i].my_list, &head_task);
 	}
 /*
 	list_for_each_entry_reverse(pos_ptr, &head_task.my_list, my_list) {
 		printf(" %d ", pos_ptr->val);	
 	}
 */
-	list_for_each_entry(pos_ptr, &head_task.my_list, my_list) {
+	list_for_each_entry(pos_ptr, &head_task, my_list) {
 		printf(" %d ", pos_ptr->val);	
 	}
 
