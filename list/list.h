e40f13451069297162850ebe4a8349e125e65d9e
Cqlismy <157823712@qq.com>
2019-07-29 15:58:26 +0800

    modified file

--- list/list.h
+++ list/list.h
@@ -44,9 +44,18 @@ void list_add(struct list_head *new, struct list_head *head)
 #define list_next_entry(pos, member)	\
 	list_entry((pos)->member.next, typeof(*(pos)), member)
 
+/**
+ *	list_for_each_entry - iterate over list of given type
+ *	@pos:	 the type * to use as a loop cursor.
+ *	@head:	 the head for your list.
+ *	@member: the name of the list_head within the struct.
+ */
 #define list_for_each_entry(pos, head, member)			\
 	for(pos = list_first_entry(head, typeof(*pos), member);		\
 		&pos->member != (head);						\
 		pos = list_next_entry(pos, member))
 
+
 #endif
+
+
