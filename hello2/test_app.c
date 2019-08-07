64fc297abc91845b7035762c73c07c9a50a1dbfd
Cqlismy <157823712@qq.com>
2019-08-07 11:34:28 +0800

    add new files

---
+++ hello2/test_app.c
@@ -0,0 +1,22 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int fd;
+
+	fd = open("/dev/hello", O_RDWR);
+	if (fd < 0) {
+		printf("open fail\n");
+		exit(-1);
+	}
+
+	printf("open success\n");
+	close(fd);
+
+	return 0;
+}
